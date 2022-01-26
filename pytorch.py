ec89be33529e6738d598a7e7d7a9f2defee48299
Vinh Phan <pvphan90@gmail.com>
2022-01-26 10:22:35 -0800

    some cleanup

--- pytorch.py
+++ pytorch.py
@@ -1,15 +1,15 @@
-# https://towardsdatascience.com/linear-regression-with-pytorch-eb6dedead817
+# original source: https://towardsdatascience.com/linear-regression-with-pytorch-eb6dedead817
+# with some style changes
 
 import numpy as np
 import matplotlib.pyplot as plt
+import torch
+from torch.autograd import Variable
 
 x = np.linspace(0, 1, 11)
 y = 2 * x
 
-#################################
 
-import torch
-from torch.autograd import Variable
 class linearRegression(torch.nn.Module):
     def __init__(self, inputSize, outputSize):
         super(linearRegression, self).__init__()
@@ -19,60 +19,56 @@ class linearRegression(torch.nn.Module):
         out = self.linear(x)
         return out
 
-###################################
 
-use_cuda = torch.cuda.is_available()
-device = torch.device("cuda" if use_cuda else "cpu")
+def main():
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
 
-inputDim = 1        # takes variable 'x'
-outputDim = 1       # takes variable 'y'
-learningRate = 0.01
-epochs = 100
+    inputDim = 1        # takes variable 'x'
+    outputDim = 1       # takes variable 'y'
+    learningRate = 0.01
+    epochs = 100
 
-model = linearRegression(inputDim, outputDim)
-model.to(device)
-##### For GPU #######
-if use_cuda:
-    model.cuda()
+    model = linearRegression(inputDim, outputDim)
+    model.to(device)
+    if use_cuda:
+        model.cuda()
 
-####################################
+    criterion = torch.nn.MSELoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=learningRate)
 
-criterion = torch.nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=learningRate)
+    inputs = torch.tensor(x, device=device, dtype=torch.float32).view(-1,1)
+    labels = torch.tensor(y, device=device, dtype=torch.float32).view(-1,1)
 
-####################################
+    for epoch in range(epochs):
+        # Clear gradient buffers because we don't want any gradient from previous epoch to carry forward, dont want to cummulate gradients
+        optimizer.zero_grad()
 
-inputs = torch.tensor(x, device=device, dtype=torch.float32).view(-1,1)
-labels = torch.tensor(y, device=device, dtype=torch.float32).view(-1,1)
+        # get output from the model, given the inputs
+        outputs = model(inputs)
 
-for epoch in range(epochs):
+        # get loss for the predicted output
+        loss = criterion(outputs, labels)
+        print(loss)
+        # get gradients w.r.t to parameters
+        loss.backward()
 
-    # Clear gradient buffers because we don't want any gradient from previous epoch to carry forward, dont want to cummulate gradients
-    optimizer.zero_grad()
+        # update parameters
+        optimizer.step()
 
-    # get output from the model, given the inputs
-    outputs = model(inputs)
+        print('epoch {}, loss {}'.format(epoch, loss.item()))
 
-    # get loss for the predicted output
-    loss = criterion(outputs, labels)
-    print(loss)
-    # get gradients w.r.t to parameters
-    loss.backward()
+    with torch.no_grad(): # we don't need gradients in the testing phase
+        predicted = model(inputs).cpu().data.numpy()
+        print(predicted)
 
-    # update parameters
-    optimizer.step()
-
-    print('epoch {}, loss {}'.format(epoch, loss.item()))
+    plt.clf()
+    plt.plot(x, y, 'go', label='True data', alpha=0.5)
+    plt.plot(x, predicted, '--', label='Predictions', alpha=0.5)
+    plt.legend(loc='best')
+    plt.show()
 
 
-#####################################
+if __name__ == "__main__":
+    main()
 
-with torch.no_grad(): # we don't need gradients in the testing phase
-    predicted = model(inputs).cpu().data.numpy()
-    print(predicted)
-
-plt.clf()
-plt.plot(x, y, 'go', label='True data', alpha=0.5)
-plt.plot(x, predicted, '--', label='Predictions', alpha=0.5)
-plt.legend(loc='best')
-plt.show()
